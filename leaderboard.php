a364157f4909754ed47c57f36788d25f70702a39
Michle99 <63488970+Michle99@users.noreply.github.com>
2021-03-21 23:52:04 -0400

    commit -m "Making the leaderboard.php page"\

---
+++ leaderboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE>
+<html>
+<head>
+    <link href="tiles.css" rel="stylesheet" />
+    <title>Leaderboard</title>
+    <style>
+       
+        body {
+            height: 100vh;
+            width: 100vw;
+            background: #05404f;
+            overflow: hidden;
+        }
+        .header {
+            display: block;
+            margin: 2rem 1rem 0 0;
+        }
+        .header a {
+            color: #05404f;
+            outline: 0;
+            background: #f2f2f2;
+            border: 0;
+            border-radius: 0.5rem;
+            padding: 0.75rem;
+            box-sizing: border-box;
+            font: 2rem  sans-serif;
+        }
+        .header p {
+            float: left;
+            margin-right: 2rem;
+            font: 2rem  sans-serif;
+            color: white;
+        }
+        .right {
+            float: right;
+        }
+    </style>
+</head>
+<?php include("heads.php");?>
+
+
+<body>
+    <h2>Leaderboard</h2>
+<div>
+    <table>
+        <tr>
+            <td>Ranking</td>
+            <td>Ranking</td>
+            <td>Ranking</td>
+        </tr>
+    </table>
+
+<?php
+
+
+?>
+
+
+
+
+
+
+</div>
+</body>
+</html>
